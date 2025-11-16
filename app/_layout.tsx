3647f2b7d7c4a728bf8c3d131f2ac136ed0ea8b4
quihp <quiquihuynh@gmail.com>
2025-11-16 16:00:13 +0700

    Fix conflict bg phat

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
 import TwinkleStars from '@/components/Background/TwinkleStarsCore';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { View, Text, ScrollView } from 'react-native'
-import {SafeAreaView} from 'react-native-safe-area-context';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import StoreProvider from '@/redux/StoreProvider';
 import 'react-native-reanimated';
 import Home from './Home/Home';
@@ -16,21 +16,21 @@ export default function RootLayout() {
 
   return (
     <SafeAreaView style={styles.layout}>
-    <TwinkleStars
-      background="#000033"
-      particleDensity={50}
-      particleColor="#FFFFFF"
-      minSize={0.5}
-      maxSize={2}
-    >
-      <View style={styles.layout}>
-        <View style={styles.container}>
-          <ScrollView contentContainerStyle={{ padding: 16 }}>
-            <Home />
-          </ScrollView>
+      <TwinkleStars
+        background="#000033"
+        particleDensity={50}
+        particleColor="#FFFFFF"
+        minSize={0.5}
+        maxSize={2}
+      >
+        <View style={styles.layout}>
+          <View style={styles.container}>
+            <ScrollView contentContainerStyle={{ padding: 16 }}>
+              <Home />
+            </ScrollView>
+          </View>
         </View>
+      </TwinkleStars>
     </SafeAreaView>
-      </View>
-    </TwinkleStars>
   );
 }
