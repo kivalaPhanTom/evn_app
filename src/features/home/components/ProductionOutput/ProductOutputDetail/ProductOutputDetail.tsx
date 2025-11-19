b9c39eb3bd8e511330866ed1546d83da24c3788b
phattt10 <phattt10@viettel.com.vn>
2025-11-19 21:07:26 +0700

    feat: add Product Output Detail

--- src/features/home/components/ProductionOutput/ProductOutputDetail/ProductOutputDetail.tsx
+++ src/features/home/components/ProductionOutput/ProductOutputDetail/ProductOutputDetail.tsx
@@ -1,15 +1,21 @@
 import AnimatedCardContainer from '@/components/AnimatedCardContainer/AnimatedCardContainer.component'
 import React, { useEffect, useRef, useState } from 'react'
-import { Text, View } from 'react-native'
 import ProductCumulativeOutput from './ProductCummulativeOutput/ProductComulativeOutput'
+import { ScrollView, Text, View } from 'react-native'
+import CompareOutput24h from './components/CompareOutput24h/CompareOutput24h'
+import style from './ProductOutputDetail.styles'
 
 export default function ProductOutputDetail() {
   return (
-    <View style={{ paddingHorizontal: 16 }}>
-      <AnimatedCardContainer>
-        <Text>So sánh sản lượng 24h</Text>
-      </AnimatedCardContainer>
-      <ProductCumulativeOutput />
-    </View>
+    <ScrollView contentContainerStyle={{ paddingBottom: 20 }}>
+      <View style={{ paddingHorizontal: 16 }}>
+        <View style={style.section}>
+          <CompareOutput24h />
+        </View>
+        <View style={style.section}>
+          <ProductCumulativeOutput />
+        </View>
+      </View>
+    </ScrollView>
   )
 }
