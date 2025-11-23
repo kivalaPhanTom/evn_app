b40572ae51ad7cc255033ac893b0a19478e8482e
phattt10 <phattt10@viettel.com.vn>
2025-11-23 13:09:58 +0700

    feat: add tab for Hydrology

--- src/features/home/components/Hydrology/Hydrology.tsx
+++ src/features/home/components/Hydrology/Hydrology.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { View } from 'react-native'
 import SectionContainer from '@/components/ui/SectionContainer/SectionContainer.component'
 import PowerStoreInLake from './PowerStoreInLake/PowerStoreInLake'
+import Overview from './Overview/Overview'
 
 function Hydrology() {
   return (
     <SectionContainer title="Thủy văn">
       <View style={{ marginBottom: 20 }}>
+        <Overview />
         <PowerStoreInLake />
       </View>
     </SectionContainer>
