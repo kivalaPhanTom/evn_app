a9423ac5f6207d2b18bc81cdf99371b4f07888d1
Duy Đình <17520398@gm.uit.edu.vn>
2025-12-02 23:35:19 +0700

    update export apk file

--- app.config.js
+++ app.config.js
@@ -1,8 +1,26 @@
+// require('dotenv').config(); // Load .env
+
+// module.exports = ({ config }) => ({
+//   ...config,
+//   name: "evn_app",
+//   slug: "evn_app",
+//   extra: {
+//     ...config.extra,
+//     API_BASE_URL: process.env.API_BASE_URL,
+//   },
+// });
 import 'dotenv/config'; // load .env tự động
 
-export default ({ config }) => ({
+module.exports = ({ config }) => ({
   ...config,
+  name: "evn_app",
+  slug: "evn_app",
+  android: {
+    ...config.android,
+    package: "com.duynguyendinh.evnapp", // <--- Bắt buộc
+  },
   extra: {
+    ...config.extra,
     API_BASE_URL: process.env.API_BASE_URL,
   },
 });
