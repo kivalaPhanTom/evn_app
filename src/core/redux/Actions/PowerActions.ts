07727ca8ebebd526916a905b0b077439b7fcd3ef
datdt44 <datdt44@viettel.com.vn>
2025-12-02 09:22:00 +0700

    add api power by time

--- src/core/redux/Actions/PowerActions.ts
+++ src/core/redux/Actions/PowerActions.ts
@@ -2,3 +2,4 @@ import { createAction } from '@reduxjs/toolkit'
 import * as ACTION_TYPES from '../ActionTypes/PowerActionTypes'
 
 export const getPowerOverivew = createAction(ACTION_TYPES.GET_POWER_OVERVIEW)
+export const getPowerByTime = createAction(ACTION_TYPES.GET_POWER_BY_TIME)
