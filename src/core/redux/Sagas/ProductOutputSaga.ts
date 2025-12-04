c81535dd78bd5f59f2486838b8ec25613d99e8be
phattt10 <phattt10@viettel.com.vn>
2025-12-04 23:54:46 +0700

    feat: implement api for Power + Product output

--- src/core/redux/Sagas/ProductOutputSaga.ts
+++ src/core/redux/Sagas/ProductOutputSaga.ts
@@ -1,7 +1,7 @@
 import { all, takeEvery, put, call } from 'redux-saga/effects'
-import { getProductOutputByHours } from '../Actions/ProductOutputActions'
+import { getProductOutputByHours, getProductOutputOverview, getProductOutputByDays } from '../Actions/ProductOutputActions'
 import { Service } from '@/core/service/productOutput'
-import { setProductOutputByHours } from '../slices/ProductOutputSlice'
+import { setProductOutputByHours, setProductOutputOverview, setProductOutputByDays } from '../slices/ProductOutputSlice'
 
 function* getProductOutputByHoursSaga(): Generator {
   try {
@@ -14,9 +14,38 @@ function* getProductOutputByHoursSaga(): Generator {
   }
 }
 
+function* getProductOutputOverviewSaga(): Generator {
+  try {
+    const res = yield call(Service.getProductOutputOverviewApi)
+    if (res.status === 200) {
+      yield put(setProductOutputOverview(res.data))
+    }
+  } catch (error) {
+    console.log('errorXXX:', error)
+  }
+}
+
+function* getProductOutputByDaysSaga(action: ReturnType<typeof getProductOutputByDays>): Generator {
+  try {
+    const n = action.payload || 7 // default is 7 days
+    const res = yield call(Service.getProductOutputByDaysApi, n)
+    if (res.status === 200) {
+      yield put(setProductOutputByDays(res.data))
+    }
+  } catch (error) {
+    console.log('errorXXX:', error)
+  }
+}
+
 function* handleGetProductOutputByHoursApi() {
   yield takeEvery(getProductOutputByHours, getProductOutputByHoursSaga)
 }
+function* handleGetProductOutputOverviewApi() {
+  yield takeEvery(getProductOutputOverview, getProductOutputOverviewSaga)
+}
+function* handleGetProductOutputByDaysApi() {
+  yield takeEvery(getProductOutputByDays, getProductOutputByDaysSaga)
+}
 export function* productOutputSagaList() {
-  yield all([handleGetProductOutputByHoursApi()])
+  yield all([handleGetProductOutputByHoursApi(), handleGetProductOutputOverviewApi(), handleGetProductOutputByDaysApi()])
 }
--- src/core/service/productOutput.ts
+++ src/core/service/productOutput.ts
@@ -3,11 +3,25 @@ import { prefix_api } from '../constants/vars'
 
 export const Service = {
   getProductOutputByHoursApi,
+  getProductOutputOverviewApi,
+  getProductOutputByDaysApi,
 }
 export const servicePattern = {
   getProductOutputByHours: `${prefix_api}/ProductByHours`,
+  getProductOutputOverview: `${prefix_api}/ProductOutputOverview`,
+  getProductOutputByDays: `${prefix_api}/Prodct_Recent_Days`,
 }
 
 function getProductOutputByHoursApi() {
   return api.get(`${servicePattern.getProductOutputByHours}`)
+}
+
+function getProductOutputOverviewApi() {
+  return api.get(`${servicePattern.getProductOutputOverview}`)
+}
+
+function getProductOutputByDaysApi(dayNumber: number = 7) {
+  return api.get(`${servicePattern.getProductOutputByDays}`, {
+    params: { N: dayNumber }
+  })
 }
