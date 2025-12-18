d980f977ac1798e1ec273d66159d431d9ce454f9
Duy Đình <17520398@gm.uit.edu.vn>
2025-12-18 21:22:13 +0700

    updating swipe to factory detail

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,3 +1,9 @@
+import React from 'react'
+import { Gesture, GestureDetector } from 'react-native-gesture-handler'
+import { Ionicons } from '@expo/vector-icons'
+import { useLocalSearchParams } from 'expo-router'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import { useRouter } from 'expo-router'
 import TwinkleStars from '@/components/Background/TwinkleStarsCore'
 import GradientText from '@/components/GradientText/GradientText.component'
 import { lightGradients } from '@/core/constants/gradients'
@@ -5,16 +11,20 @@ import { textGradients } from '@/core/constants/gradients'
 import { px } from '@/core/utils/scale'
 import PowerSection from '@/features/home/components/PowerSection/PowerSection'
 import ProductionOutput from '@/features/home/components/ProductionOutput/ProductionOutput'
-import { Ionicons } from '@expo/vector-icons'
-import { useLocalSearchParams } from 'expo-router'
-import React from 'react'
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import Hydrology from '@/features/home/components/Hydrology/Hydrology'
-interface Props {}
+
+interface Props { }
 
 function HomeNewScreen(props: Props) {
-  const {} = props
-
+  const { } = props
+  const router = useRouter()
+  const swipeLeft = Gesture.Pan()
+    .activeOffsetX([-30, 30]) // CHỈ nhận vuốt ngang
+    .onEnd(e => {
+      if (e.translationX < -80) {
+        router.push('/factory-detail/index')
+      }
+    })
   const { companyName, location } = useLocalSearchParams<{
     companyName?: string | string[]
     location?: string | string[]
@@ -23,25 +33,29 @@ function HomeNewScreen(props: Props) {
   const companyLocation = Array.isArray(location) ? location[0] : location
 
   return (
-    <TwinkleStars background="#000033" particleDensity={50} particleColor="#FFFFFF" minSize={0.5} maxSize={2}>
-      <View style={styles.header}>
-        <GradientText
-          text={companyTitle ?? 'CÔNG TY THỦY ĐIỆN BUÔN KUỐP'}
-          colors={textGradients.water}
-          fontSize={px.f(30)}
-          style={{ textAlign: 'center' }}
-        />
-        <View style={styles.locationRow}>
-          <Ionicons name="location" size={px.f(12)} color="#FF6A6A" style={{ marginRight: px.h(6) }} />
-          <Text style={styles.locationText}>{companyLocation ?? 'Đắk Lắk, Việt Nam'}</Text>
-        </View>
+    // <GestureDetector gesture={swipeLeft}>
+      <View style={{ flex: 1 }} collapsable={false}>
+        <TwinkleStars background="#000033" particleDensity={50} particleColor="#FFFFFF" minSize={0.5} maxSize={2}>
+          <View style={styles.header}>
+            <GradientText
+              text={companyTitle ?? 'CÔNG TY THỦY ĐIỆN BUÔN KUỐP'}
+              colors={textGradients.water}
+              fontSize={px.f(30)}
+              style={{ textAlign: 'center' }}
+            />
+            <View style={styles.locationRow}>
+              <Ionicons name="location" size={px.f(12)} color="#FF6A6A" style={{ marginRight: px.h(6) }} />
+              <Text style={styles.locationText}>{companyLocation ?? 'Đắk Lắk, Việt Nam'}</Text>
+            </View>
+          </View>
+          <ScrollView>
+            <PowerSection />
+            <ProductionOutput />
+            <Hydrology />
+          </ScrollView>
+        </TwinkleStars>
       </View>
-      <ScrollView>
-        <PowerSection />
-        <ProductionOutput />
-        <Hydrology />
-      </ScrollView>
-    </TwinkleStars>
+    // </GestureDetector>
   )
 }
 
