b788d5bdde1935ec1d6a087065c775b27421e4e7
quihp <quiquihuynh@gmail.com>
2026-01-22 08:39:41 +0700

    Feat: Sửa lỗi gọi API định kỳ sản lượng liên tục + Chuyển các biểu đồ line curve -> straight, + fix lỗi \u2211

--- app/factory-detail/ProductionOutputFactDetail/TotalProductionOutputFactDetail/TotalProductionOutputFactDetail.tsx
+++ app/factory-detail/ProductionOutputFactDetail/TotalProductionOutputFactDetail/TotalProductionOutputFactDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getProductOutputOverviewFactDetail } from '@/core/redux/Actions/ProductOutputActions'
 import { RootState } from '@/core/redux/store'
@@ -30,8 +30,7 @@ function TotalProductionOutputFactDetail(props: Props) {
   const [powerSources, setPowerSources] = useState<powerSources[]>([])
   const { activeTabIndex } = useSelector((state: RootState) => state.powerSlice)
   const { countRefesh } = useSelector((state: any) => state.factoryDetailSlice)
-
-  useAlignedHourlyTimer(() => {
+  const timerCallback = useCallback(() => {
     if (activeTabIndex === keyTab) {
       dispatch(
         getProductOutputOverviewFactDetail({
@@ -41,7 +40,8 @@ function TotalProductionOutputFactDetail(props: Props) {
         }),
       )
     }
-  })
+  }, [activeTabIndex, keyTab, dispatch])
+  useAlignedHourlyTimer(timerCallback)
 
   useEffect(() => {
     if (activeTabIndex === keyTab) {
