b47ac70ad3176c6a07dac2a9c815fd67051261d6
quihp <quiquihuynh@gmail.com>
2026-03-01 21:59:47 +0700

    Feat: Thêm thư viện react-pdf-view riêng cho android, thư viện này không hỗ trợ expo nên tách khỏi nhánh chính khi nào cần build thì merge từ master vào

--- src/components/PDFViewer/PDFViewer.styles.ts
+++ src/components/PDFViewer/PDFViewer.styles.ts
@@ -100,5 +100,9 @@ const styles = StyleSheet.create({
         fontSize: 22,
         color: '#333',
     },
+    pdf: {
+        flex: 1,
+        width: '100%',
+    },
 });
 export default styles;
